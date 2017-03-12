0b647d5a12dbaf99578604ea841b00b2ae9ec8c8
Alexa Bagnard <abagnard@gmail.com>
2017-03-12 12:27:57 -0700

    removed getNumberOfSongs()

--- nb.js
+++ nb.js
@@ -37,13 +37,10 @@ function train(chords, label){
    }
 };
 
-function getNumberOfSongs(){
- return songs.length;
-};
 
 function setLabelProbabilities(){
  Object.keys(labelCounts).forEach(function(label){
-   var numberOfSongs = getNumberOfSongs();
+   var numberOfSongs = songs.length;
    labelProbabilities[label] = labelCounts[label] / numberOfSongs;
  });
 };
@@ -55,8 +52,7 @@ function setChordCountsInLabels(){
    }
   song[1].forEach(function(chord){
     if(chordCountsInLabels[song[0]][chord] > 0){
-      chordCountsInLabels[song[0]][chord] =
-      chordCountsInLabels[song[0]][chord] + 1;
+      chordCountsInLabels[song[0]][chord] += 1;
     } else {
       chordCountsInLabels[song[0]][chord] = 1;
     }
@@ -68,8 +64,7 @@ function setProbabilityOfChordsInLabels(){
   probabilityOfChordsInLabels = chordCountsInLabels;
   Object.keys(probabilityOfChordsInLabels).forEach(function(difficulty){
     Object.keys(probabilityOfChordsInLabels[difficulty]).forEach(function(chord){
-      probabilityOfChordsInLabels[difficulty][chord] =
-      probabilityOfChordsInLabels[difficulty][chord] / songs.length;
+      probabilityOfChordsInLabels[difficulty][chord] /= songs.length;
     });
   });
 }
