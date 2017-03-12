961e968d4d324563c572496799d4fce81353bc1e
Alexa Bagnard <abagnard@gmail.com>
2017-03-12 14:52:36 -0700

    created Word class with eng and jap subclass

---
+++ word.js
@@ -0,0 +1,21 @@
+class Word{
+  constructor(word){
+    this.word = word;
+  }
+
+  count(){
+    return this.word.length;
+  };
+};
+
+class EnglishWord extends Word{}
+
+
+class JapaneseWord extends Word{}
+
+const japaneseWord =  new JapaneseWord("犬");
+const englishWord = new EnglishWord("dog");
+console.log(japaneseWord.word);
+console.log(japaneseWord.count());
+console.log(englishWord.word);
+console.log(englishWord.count());
